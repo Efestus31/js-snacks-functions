e5bbedc52d3e0e823e2ce4c636da19dd9e74f69a
Efestus31 <biagiohd@gmail.com>
2024-10-07 15:25:33 +0200

    esercizio conta vocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,22 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function getVowels(string){
+    let vowels ='aAeEiIoOuU';
+    let vowelscount = 0;
+    for( let i = 0; i <word.length; i++){
+        if (vowels.indexOf(word[i]) !== -1){
+            vowelscount += 1;
+        }
+    }
+    return vowelscount
+}
+const vowelsResult = getVowels()
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(vowelsResult);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
