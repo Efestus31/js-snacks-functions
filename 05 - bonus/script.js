8b25fe980dac9d23136da6a749b0253ed5aff918
Efestus31 <biagiohd@gmail.com>
2024-10-07 16:01:37 +0200

    esercizio bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -9,9 +9,21 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function greetingByTime(name){
+    const hour = new Date().getHours;
+    let greeting;
+    if(hour < 13){
+        greeting = 'Buongiorno';
+    }else if (hour < 17){
+        greeting = 'Buonpomeriggio';
+    } else {
+        greeting = 'Buonasera';
+    }
+    return greeting + ' ' + name + '!';
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(greetingByTime(name));
 
 
 
